1955d4c1ce1ed41d8babc94834fb92503f03e792
Pranavi Reddi <reddi.pranavi@gmail.com>
2026-04-05 21:07:15 -0400

    testing changes for deploying

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, send_from_directory
 from generator import PuzzleGenerator
 
 app = Flask(__name__)
 generator = PuzzleGenerator("processed_categories.json")
+
+@app.get("/")
+def home():
+    return send_from_directory("public", "index.html")
 
 @app.get("/api/puzzle")
 def get_puzzle():
